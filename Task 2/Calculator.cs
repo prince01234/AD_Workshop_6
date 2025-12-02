613b608baba7cf21bba6b15698320eecad88354d
Prince <princeshrestha2111018@gmail.com>
2025-12-02 12:22:17 +0545

    Task 2 completed

---
+++ Task 2/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+public delegate int Calculate(int a, int b);
+public delegate double DiscountStrategy(double price);
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+    
+    public double FestivalDiscount(double price) => price * 0.8;
+    public double SeasonalDiscount(double price) => price * 0.9;
+    public double NoDiscount(double price) => price;
+}
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+            public static void Main(string[] args)
+            {
+                Calculator calc = new Calculator();
+
+                Console.WriteLine("Addition: " + calc.Add(8, 3));
+                Console.WriteLine("Subtraction: " + calc.Subtract(16, 4));
+
+                double price = 3000;
+                Console.WriteLine("\nOriginal Price: " + price);
+                DiscountStrategy discount;
+
+                discount = calc.FestivalDiscount;
+                Console.WriteLine("Price after Festival Discount: " + discount(price));
+
+                discount = calc.SeasonalDiscount;
+                Console.WriteLine("Price after Seasonal Discount: " + discount(price));
+
+                discount = calc.NoDiscount;
+                Console.WriteLine("Price with No Discount: " + discount(price));
+            }
+    }
+}
