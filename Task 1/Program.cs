5ef040440a5e4ac4e6e07d4b9d4240991b3fb4c3
Prince <princeshrestha2111018@gmail.com>
2025-12-02 11:13:50 +0545

    Task 1 successfully completed

---
+++ Task 1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Length = 5;
+            rectangle.Width = 3;
+
+            Console.WriteLine("Rectangle Details:");
+            Console.WriteLine(rectangle.ShowDetails());
+            Console.WriteLine($"Area: {rectangle.GetArea()}");
+            Console.WriteLine($"Perimeter: {rectangle.GetPerimeter()}");
+        }
+    }
+}
---
+++ Task 1/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public double Length
+        {
+            get { return length; }
+            set {  length = value; }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public double GetArea() => length * width;
+
+        public double GetPerimeter() => 2 * (length + width);
+
+        public string ShowDetails() => $"Length: {length}, Width: {width}";
+    }
+}
